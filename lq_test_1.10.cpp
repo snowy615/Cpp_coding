d0ed273441d8a02f15d902c9f615f799f73e5376
snowy615 <yansnow615@gmail.com>
2025-07-21 16:31:32 -0700

    misunderstood problem 2

--- lq_test_1.10.cpp
+++ lq_test_1.10.cpp
@@ -6,19 +6,25 @@ int a[N][N];
 int s;
 int xa, ya, xb, yb, z;
 int main(){
-    s = 0;
+
     cin >> n >> m >> c;
     for (int i=1; i<=n; i++){
         for (int j=1; j<=m; j++){
             cin >> a[i][j];
-            s += a[i][j];
+            a[i][j] += a[i][j-1];
+            cout << a[i][j] << " ";
         }
+        cout << "\n";
     }
     cin >> t;
-    cout << s << "\n";
     for (int i=1; i<=t; i++){
+        s = 0;
         cin >> xa >> ya >> xb >> yb >> z;
         if (z>c) z%=c;
+        for (int j=ya; j<=yb; j++){
+            cout << "\n new part s " << a[xb]-a[xa-1];
+            s += a[j][xb]-a[j][xa-1];
+        }
         s -= (xb-xa+1)*(yb-ya+1)*z;
         cout << s << "\n";
     }
