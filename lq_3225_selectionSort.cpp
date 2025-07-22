36a29455c0d2e8b532380da265e650b69aa23211
snowy615 <yansnow615@gmail.com>
2025-07-22 12:32:19 -0700

    selection sort

---
+++ lq_3225_selectionSort.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int n;
+const int N=1e3+5;
+int a[N];
+
+int main(){
+    cin >> n;
+    for (int i=1; i<=n; i++) cin >> a[i];
+
+    for (int i=n; i>=1; i--){
+        int m =1;
+        for (int j=1; j<=i; j++) if (a[j] > a[m]) m = j;
+        swap(a[i], a[m]);
+    }
+    for (int i=1; i<=n; i++) cout << a[i] << " ";
+    return 0;
+}
