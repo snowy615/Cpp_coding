39bf12463d8be43c4de7a7791a59ea42acf1a3be
snowy615 <yansnow615@gmail.com>
2025-07-22 11:26:26 -0700

    merge sort 2nd time coding

--- lq_3226_mergeSort.cpp
+++ lq_3226_mergeSort.cpp
@@ -1,30 +1,31 @@
 #include <iostream>
 using namespace std;
-int n;
+
 const int N = 1e5+5;
+int n;
 int a[N], b[N];
 
-void mergeSort(int l, int r){
-    if (l == r) return;
-
-    int mid = l+r >> 1;
-    mergeSort(l, mid);
-    mergeSort(mid+1, r);
-    int pl = l, pr = mid+1, pb = l;
-    while (pl <= mid || pr <= r){
-        if (pl > mid) b[pb++] = a[pr++];
-        else if (pr > r) b[pb ++] = a[pl++];
+void ms(int l, int r){
+    if (l==r) return;
+    int mid, pl, pr, pb;
+    mid = (l+r) >> 1;
+    ms(l, mid);
+    ms( mid+1, r);
+    pl = l, pr = mid+1, pb=l;
+    while(pl<=mid || pr<=r){
+        if (pl>mid) b[pb++] = a[pr++];
+        else if (pr>r) b[pb++] = a[pl++];
         else{
-            if (a[pl] < a[pr]) b[pb ++] = a[pl++];
-            else b[pb ++] = a[pr ++];
+            if (a[pl] <= a[pr]) b[pb ++] = a[pl ++];
+            else b[pb++] = a[pr++];
         }
     }
     for (int i=l; i<=r; i++) a[i] = b[i];
-
 }
 int main(){
     cin >> n;
     for (int i=1; i<=n; i++) cin >> a[i];
-    mergeSort(1,n);
+    ms(1, n);
     for (int i=1; i<=n; i++) cout << a[i] << " ";
+    return 0;
 }
