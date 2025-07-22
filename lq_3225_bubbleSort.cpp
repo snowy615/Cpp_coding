d0fc409b02d440d5d25cd62065543eafc4923a9a
snowy615 <yansnow615@gmail.com>
2025-07-22 12:26:08 -0700

    BUBBLE 2nd time

--- lq_3225_bubbleSort.cpp
+++ lq_3225_bubbleSort.cpp
@@ -7,12 +7,12 @@ int a[N];
 int main(){
     cin >> n;
     for (int i=1; i<=n; i++) cin >> a[i];
-
-    for (int i=n; i >= 1; i--){
-        for (int j=1; j<i; j++) {
+    for (int i=n; i>=1; i--){
+        for (int j=1; j<i; j++){
             if (a[j] > a[j+1]) swap(a[j], a[j+1]);
         }
     }
+
     for (int i=1; i<=n; i++) cout << a[i] << " ";
     return 0;
 }
