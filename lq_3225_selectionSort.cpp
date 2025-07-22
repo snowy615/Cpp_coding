11fe16949223d2f22589b9ed71d33113e15b3add
snowy615 <yansnow615@gmail.com>
2025-07-22 12:35:44 -0700

    selection sort 2nd time

--- lq_3225_selectionSort.cpp
+++ lq_3225_selectionSort.cpp
@@ -10,10 +10,13 @@ int main(){
     for (int i=1; i<=n; i++) cin >> a[i];
 
     for (int i=n; i>=1; i--){
-        int m =1;
-        for (int j=1; j<=i; j++) if (a[j] > a[m]) m = j;
-        swap(a[i], a[m]);
+        int m = i;
+        for (int j=1; j<=i; j++){
+            if (a[j] > a[m]) m=j;
+        }
+        swap(a[m], a[i]);
     }
+
     for (int i=1; i<=n; i++) cout << a[i] << " ";
     return 0;
 }
