f034c551ed86ba0ba89228527b015ef9af735c8d
snowy615 <yansnow615@gmail.com>
2025-07-22 14:34:34 -0700

    12 TLE. attempt to fix it.

--- lq_test_1.12.cpp
+++ lq_test_1.12.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 const int N=2e5+5;
+int ps[N];
 int shift[N];
 
 int main(){
@@ -12,11 +13,14 @@ int main(){
     cin >> s;
     for (int i=1; i<=q; i++){
         cin >> l >> r >> k;
-        for (int j=l; j<=r; j++) {
-            shift[j] = (shift[j]+k)%26;
-            //cout << j << "j "<< shift[j] << "\n";
-        }
+        k = k%26;
+        ps[l] += k;
+        ps[r+1] -= k;
     }
+//    for (int i=1; i<=n; i++) cout << ps[i] << " ";
+//    cout << "\n";
+    for (int i=1; i<=n; i++) shift[i] = shift[i-1]+ps[i];
+
     for (int i=1; i<=n; i++){
         //cout << "\n "<< s[i-1] << "\n";
         char c = 'a'+ (s[i-1] -'a'+ shift[i])%26;
