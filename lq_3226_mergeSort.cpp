da03096f7a1cab11e9c9770d0d631a2f119aa773
snowy615 <yansnow615@gmail.com>
2025-07-22 11:05:03 -0700

    merge sort

--- lq_3226_mergeSort.cpp
+++ lq_3226_mergeSort.cpp
@@ -4,12 +4,12 @@ int n;
 const int N = 1e5+5;
 int a[N], b[N];
 
-void mergeSort(int a[], int l, int r){
+void mergeSort(int l, int r){
     if (l == r) return;
 
     int mid = l+r >> 1;
-    mergeSort(a, l, mid);
-    mergeSort(a, mid+1, r);
+    mergeSort(l, mid);
+    mergeSort(mid+1, r);
     int pl = l, pr = mid+1, pb = l;
     while (pl <= mid || pr <= r){
         if (pl > mid) b[pb++] = a[pr++];
@@ -25,6 +25,6 @@ void mergeSort(int a[], int l, int r){
 int main(){
     cin >> n;
     for (int i=1; i<=n; i++) cin >> a[i];
-    mergeSort(a, 1,n);
+    mergeSort(1,n);
     for (int i=1; i<=n; i++) cout << a[i] << " ";
 }
