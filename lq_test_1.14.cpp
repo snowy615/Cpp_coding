1d86f04c5237b539a92351e9b93773c2cde11f4f
snowy615 <yansnow615@gmail.com>
2025-07-25 10:25:22 -0700

    passed 10/10. Problem solved was cnt and tbase out of range.

--- lq_test_1.14.cpp
+++ lq_test_1.14.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <cmath>
 #include <algorithm>
+using ll = long long;
 using namespace std;
-int n;
+ll n;
 const int N=1e5+5;
-int ma, mb;
-int a[N], b[N];
+ll ma, mb;
+ll a[N], b[N];
 
 int main(){
-    int cnt = 0, bd = 0;
+    ll cnt = 0, bd = 0;
     cin >> n;
     cin >> ma;
     for (int i=1; i<=ma; i++) cin >> a[i];
@@ -16,24 +17,28 @@ int main(){
     cin >> mb;
     for (int i=1; i<=mb; i++) cin >> b[i];
     reverse(b+1, b+mb+1);
-    int total_digit = max(ma, mb);
-    int tbase = 1;
+    ll total_digit = max(ma, mb);
+    ll tbase = 1;
+    ll v = 2;
     for (int i=1; i<=total_digit; i++){
-        int s = a[i]-b[i]-bd;
-        int base = max(max(max(a[i]+1, b[i]+1), s+1), 2);
+        ll s = a[i]-b[i]-bd;
+        ll base = max(max(a[i]+1, b[i]+1), max(s+1, v));
 
         if (s>=0){
             cnt += tbase*s;
+            cnt %=1000000007;
             //cout << "0 _ i " << i << " s " << s << " cnt " << tbase*s << " tbase " << tbase << "\n";
             bd = 0;
         }else{
             cnt += tbase*(s+base);
+            cnt %=1000000007;
             //cout << "0 _ i " << i << " s " << s << " cnt " << tbase*(s+base) << " tbase " << tbase << "\n";
             bd = 1;
         }
         tbase *= base;
+        tbase %= 1000000007;
     }
-    cout << cnt;
+    cout << cnt % 1000000007;
     return 0;
 
 }
