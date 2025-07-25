358c09809d2d78bd8446201c00e3bc5a662f7139
snowy615 <yansnow615@gmail.com>
2025-07-24 20:21:52 -0700

    finished 14 but weird numbers

--- lq_test_1.14.cpp
+++ lq_test_1.14.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cmath>
+#include <algorithm>
 using namespace std;
 int n;
 const int N=1e5+5;
@@ -6,17 +8,30 @@ int ma, mb;
 int a[N], b[N];
 
 int main(){
-    int cnt = 0;
+    int cnt = 0, bd = 0;
     cin >> n;
     cin >> ma;
     for (int i=1; i<=ma; i++) cin >> a[i];
-    reverse(a+1, a+ma+1);
+    //reverse(a+1, a+ma+1);
     cin >> mb;
     for (int i=1; i<=mb; i++) cin >> b[i];
-    reverse(b+1, b+mb+1);
+    //reverse(b+1, b+mb+1);
+    int total_digit = max(ma, mb);
+    for (int i=1; i<=total_digit; i++){
+        int s = a[i]-b[i]-bd;
+        int base = max(max(a[i]+1, b[i]+1), s);
 
-    for (int i=1; i<=max(ma, mb); i++){
-        int m =
+        if (s>=0){
+            cnt += pow(base,i-1)*s;
+            cout << "0 _ i " << i << " s " << s << " cnt " << pow(base,i-1)*s << "\n";
+            bd = 0;
+        }else{
+            cnt += pow(base, i-1)*(s+base);
+            cout << "0 _ i " << i << " s " << s << " cnt " << pow(base, i-1)*(s+base)<< "\n";
+            bd = 1;
+        }
     }
+    cout << cnt;
+    return 0;
 
 }
