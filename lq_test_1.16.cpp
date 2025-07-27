73204f5a8962a13aea82f509e953843909faf1e4
snowy615 <yansnow615@gmail.com>
2025-07-27 15:06:37 -0700

    1/10 passed

--- lq_test_1.16.cpp
+++ lq_test_1.16.cpp
@@ -4,23 +4,35 @@
 using namespace std;
 using ll = long long;
 ll k, n, vv,tt;
-vector<pair<ll, ll>> v;
-//
+const int N = 1e5+10;
+ll num[N];
+
+void update(int index, ll a){
+    num[index] = a;
+    if (index-1 > 0 && num[index-1] > a+1) {
+        //cout << "a";
+        update(index-1, a+1);
+    }
+    if (index+1 <= n && num[index+1] > a+1) {
+        //cout << "b";
+        update(index+1, a+1);
+    }
+}
 int main(){
     cin >> k >> n;
+    for (int i=1; i<=n; i++){
+        num[i] = 100010;
+    }
     for (int i=1; i<=k; i++){
         cin >> vv >> tt;
-        v.push_back({vv, tt});
+        update(vv, tt);
     }
     ll t_max = 0;
     for (int i=1; i<=n; i++){
-        ll cur_min = 1e15;
-        for (int j=1; j<=k; j++){
-            ll cur = abs(i-v[j].first) + v[j].second;
-            if (cur < cur_min) cur_min = cur;
-        }
-        t_max = max(t_max, cur_min);
+        if (num[i] > t_max) t_max = num[i];
+        //cout << num[i] << " ";
     }
+    //cout << "\n";
     cout << t_max;
     return 0;
 }
