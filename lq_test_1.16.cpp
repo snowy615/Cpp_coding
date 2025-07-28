47a57b0d0888e9459df31051e139855bdb589621
snowy615 <yansnow615@gmail.com>
2025-07-27 17:21:07 -0700

    weird

--- lq_test_1.16.cpp
+++ lq_test_1.16.cpp
@@ -4,16 +4,16 @@
 #include <algorithm>
 using namespace std;
 using ll = long long;
-ll k, n, vv,tt;
+ll n, len, vv,tt;
 vector<pair<ll, ll>> num;
 vector<pair<ll, ll>> x;
 
 bool check(int t){
     x.clear();
-    for (int i=0; i<k; i++) {
+    for (int i=0; i<n; i++) {
         if (num[i].second <= t) {
             ll nl = max(1ll, num[i].first - (t - num[i].second));
-            ll nr = min(n, num[i].first + (t - num[i].second));
+            ll nr = min(len, num[i].first + (t - num[i].second));
             x.push_back({nl, nr});
         }
 
@@ -22,16 +22,16 @@ bool check(int t){
     if (x[0].first > 1) return 0;
     ll end = x[0].second;
     for (int i=1; i<x.size(); i++){
-        if (x[i].first > end) break;
+        if (x[i].first > end+1) break;
         else end = max(end, x[i].second);
     }
-    return end >= n;
+    return end >= len;
 
 }
 
 int main(){
-    cin >> k >> n;
-    for (int i=0; i<k; i++){
+    cin >> n >> len;
+    for (int i=0; i<n; i++){
         cin >> vv >> tt;
         num.push_back({vv, tt});
     }
