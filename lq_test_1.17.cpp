83230f5417a85624ea477f3e0adb5ec147e6b543
snowy615 <yansnow615@gmail.com>
2025-07-30 14:07:44 -0700

    removed heap

--- lq_test_1.17.cpp
+++ lq_test_1.17.cpp
@@ -3,50 +3,32 @@
 #include <queue>
 using ll = long long;
 using namespace std;
-
-priority_queue <ll> c_max;
-priority_queue <ll, vector<ll>, greater<ll> > c_min;
+const int N = 1e5+5;
+ll c[N];
 
 int main(){
     ll n, q, k, m, v;
     cin >> n >> q;
     for (int i=1; i<=n; i++) {
-        cin >> v;
-        c_max.push(v);
-        c_min.push(v);
+        cin >> c[i];
     }
+    sort(c+1, c+n+1);
 
     for (int i=1; i<=q; i++){
         cin >> k >> m;
         ll lf = 0;
-        priority_queue <ll> cc_max = c_max;
-        priority_queue <ll, vector<ll>, greater<ll> > cc_min = c_min;
-        //cout << "s " << cc_max.size() << "\n";
-
-        while (m && !cc_max.empty() && !cc_min.empty()){
-            ll cur_max = cc_max.top();
-            ll cur_min = cc_min.top();
+        int l = 1, r = n;
+        while (m && l<r){
+            ll cur_max = c[r];
+            ll cur_min = c[l];
             if (cur_min>=k || 2*k-cur_max <= cur_min) {
                 lf += 2*k-cur_max;
-                cc_max.pop();
-            } else{
+                r--;
+            }else{
                 lf += cur_min;
-                cc_min.pop();
+                l++;
             }
             m --;
-        }
-        while (m && !cc_max.empty()){
-            ll cur_max = cc_max.top();
-            lf += 2*k-cur_max;
-            cc_max.pop();
-            m--;
-
-        }
-        while (m && !cc_min.empty()){
-            ll cur_min = cc_min.top();
-            lf += cur_min;
-            cc_min.pop();
-            m--;
         }
         cout << lf << "\n";
 
