33664b3e27aa921db46faf3126720193d4538ad3
snowy615 <yansnow615@gmail.com>
2025-07-30 14:43:15 -0700

    binary

--- lq_test_1.17.cpp
+++ lq_test_1.17.cpp
@@ -1,35 +1,31 @@
 #include <iostream>
 #include <algorithm>
-#include <queue>
 using ll = long long;
 using namespace std;
-const int N = 1e5+5;
-ll c[N];
+const ll N = 1e5+5;
+ll c[N], p[N];
 
 int main(){
-    ll n, q, k, m, v;
+    ll n, q, k, m;
     cin >> n >> q;
     for (int i=1; i<=n; i++) {
         cin >> c[i];
+        p[i] = p[i-1] + c[i];
     }
     sort(c+1, c+n+1);
 
     for (int i=1; i<=q; i++){
         cin >> k >> m;
-        ll lf = 0;
-        int l = 1, r = n;
-        while (m && l<r){
-            ll cur_max = c[r];
-            ll cur_min = c[l];
-            if (cur_min>=k || 2*k-cur_max <= cur_min) {
-                lf += 2*k-cur_max;
-                r--;
-            }else{
-                lf += cur_min;
-                l++;
-            }
-            m --;
+
+        //cheap paintings l=0, expensive paintings r=
+        ll l = 0, r = m+1;
+        while (l+1!=r){
+            ll mid = l+r >> 1;
+            if (c[mid] < 2*k-c[n-m+mid]) l = mid;
+            else r = mid;
         }
+        // cheap (l) + dif = (2k-c[i])
+        ll lf = p[l] + 2*k*(m-l) - (p[n]-p[n-m+l]);
         cout << lf << "\n";
 
     }
