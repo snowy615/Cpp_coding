c945f37fdfc7cf3cc2ab4b45fafb0f52ffdd2c55
snowy615 <yansnow615@gmail.com>
2025-07-30 13:40:38 -0700

    very large numbers inputted. Tried using ll

--- lq_test_1.17.cpp
+++ lq_test_1.17.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <algorithm>
 #include <queue>
+using ll = long long;
 using namespace std;
 
-priority_queue <int> c_max;
-priority_queue <int, vector<int>, greater<int> > c_min;
+priority_queue <ll> c_max;
+priority_queue <ll, vector<ll>, greater<ll> > c_min;
 
 int main(){
-    int n, q, k, m, v;
+    ll n, q, k, m, v;
     cin >> n >> q;
     for (int i=1; i<=n; i++) {
         cin >> v;
@@ -17,11 +18,11 @@ int main(){
 
     for (int i=1; i<=q; i++){
         cin >> k >> m;
-        int lf = 0;
+        ll lf = 0;
 
         while (m && !c_max.empty() && !c_min.empty()){
-            int cur_max = c_max.top();
-            int cur_min = c_min.top();
+            ll cur_max = c_max.top();
+            ll cur_min = c_min.top();
             if (cur_min>=k || 2*k-cur_max <= cur_min) {
                 lf += 2*k-cur_max;
                 c_max.pop();
@@ -32,13 +33,13 @@ int main(){
             m --;
         }
         while (m && c_max.empty()){
-            int cur_min = c_min.top();
+            ll cur_min = c_min.top();
             lf += cur_min;
             c_min.pop();
             m--;
         }
         while (m && c_min.empty()){
-            int cur_max = c_max.top();
+            ll cur_max = c_max.top();
             lf += 2*k-cur_max;
             c_max.pop();
             m--;
