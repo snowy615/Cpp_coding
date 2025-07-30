027a0596edc52bb93d3f706c9e9dccacd91249ce
snowy615 <yansnow615@gmail.com>
2025-07-30 13:31:34 -0700

    picture only sold once

--- lq_test_1.17.cpp
+++ lq_test_1.17.cpp
@@ -2,18 +2,47 @@
 #include <algorithm>
 using namespace std;
 
-const int N = 1e5+5;
-int c[N];
+priority_queue <int> c_max;
+priority_queue <int, vector<int>, greater<int> > c_min;
+
 int main(){
-    int n, q, k, m;
+    int n, q, k, m, v;
     cin >> n >> q;
-    for (int i=1; i<=n; i++) cin >> c[i];
-    int *c_max = max_element(c+1, c+n+1);
-    int *c_min = min_element(c+1, c+n+1);
+    for (int i=1; i<=n; i++) {
+        cin >> v;
+        c_max.push(v);
+        c_min.push(v);
+    }
 
     for (int i=1; i<=q; i++){
         cin >> k >> m;
-        //*c_max>k &&
-        if (*c_max-k>*c_min-k) cout << m*(2k-*c)
+        int lf = 0;
+
+        while (m && !c_max.empty() && !c_min.empty()){
+            int cur_max = c_max.top();
+            int cur_min = c_min.top();
+            if (cur_min>=k || 2*k-cur_max <= cur_min) {
+                lf += 2*k-cur_max;
+                c_max.pop();
+            } else{
+                lf += cur_min;
+                c_min.pop();
+            }
+            m --;
+        }
+        while (m && c_max.empty()){
+            int cur_min = c_min.top();
+            lf += cur_min;
+            c_min.pop();
+            m--;
+        }
+        while (m && c_min.empty()){
+            int cur_max = c_max.top();
+            lf += 2*k-cur_max;
+            c_max.pop();
+            m--;
+        }
+        cout << lf;
+        return 0;
     }
 }
