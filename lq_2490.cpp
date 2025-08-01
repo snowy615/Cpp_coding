df00a8703b9aa59619de1bc0fb346b44a4cf9e3c
snowy615 <yansnow615@gmail.com>
2025-07-31 17:42:34 -0700

    stack 2nd method

--- lq_2490.cpp
+++ lq_2490.cpp
@@ -1,20 +1,41 @@
+//stack approach
+//#include <iostream>
+//using namespace std;
+//char b[110];
+//
+//int main(){
+//    int n, top = 0;
+//    cin >> n;
+//    char v;
+//    for (int i=1; i<=n; i++){
+//        cin >> v;
+//        if (v==')' && top > 0 && b[top] == '(') top --;
+//        else {
+//            top ++;
+//            b[top] = v;
+//        }
+//    }
+//    if (top) cout << "No";
+//    else cout << "Yes";
+//
+//}
+
+//number approach
 #include <iostream>
 using namespace std;
-char b[110];
 
 int main(){
-    int n, top = 0;
+    int n;
     cin >> n;
     char v;
+    int cnt = 0;
+    bool t = 1;
     for (int i=1; i<=n; i++){
         cin >> v;
-        if (v==')' && top > 0 && b[top] == '(') top --;
-        else {
-            top ++;
-            b[top] = v;
-        }
+        if (v=='(') cnt ++;
+        else if (v==')' && cnt > 0) cnt --;
+        else t = 0;
     }
-    if (top) cout << "No";
-    else cout << "Yes";
-
+    if (t && cnt ==0) cout << "Yes";
+    else cout << "No";
 }
