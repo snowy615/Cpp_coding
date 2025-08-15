f53cffc20fc2c4728fe122b368e1b35012ca8a98
snowy615 <yansnow615@gmail.com>
2025-08-15 10:29:05 -0700

    2nd

--- lq_test_1.20.cpp
+++ lq_test_1.20.cpp
@@ -1,22 +1,32 @@
 #include <iostream>
 #include <cmath>
+#include <algorithm>
+#include <vector>
 
 #define int long long
 using namespace std;
 const int N=1e3+5;
 
 int n;
-int pre[N];
-
+int pre[N], a[N];
+vector <int> v;
 signed main(){
     cin >> n;
     for (int i=1; i<=n; i++) {
-        cin >> pre[i];
-        pre[i] += pre[i-1];
+        cin >> a[i];
     }
-    int md=pre[n];
     for (int i=1; i<=n; i++){
-        int d= abs(pre[n]-2*pre[i]);
+        pre[i] = pre[i-1]+a[i];
+    }
+    for (int i=1; i<=n; i++){
+        for (int j=i; j<=n; j++){
+            v.push_back(pre[j]-pre[i-1]);
+        }
+    }
+    sort(v.begin(), v.end());
+    int md=1e9+5;
+    for (int i=1; i<n; i++){
+        int d= abs(v[i]-v[i-1]);
         if (d<md) md = d;
     }
     cout << md;
