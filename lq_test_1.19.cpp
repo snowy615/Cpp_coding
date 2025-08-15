881e1edb524d7d644ad5bc9b5e0790a1e8c59fad
snowy615 <yansnow615@gmail.com>
2025-08-14 22:30:45 -0700

    6th

--- lq_test_1.19.cpp
+++ lq_test_1.19.cpp
@@ -4,13 +4,15 @@ using namespace std;
 using ll = long long;
 const int N = 1e5+5;
 int n, k, T;
-ll s[N];
+int s[N];
+int sc[N];
 
 
 //var = E(x^2) - [E(x)]^2
 bool check(int mid){
+    for (int i=1; i<=mid; i++) sc[i] = s[i];
     ll first[mid+1], second[mid+1];
-    sort(s, s+mid+1);
+    sort(sc+1, sc+mid+1);
     for (int i=1; i<=n; i++){
         first[i] = first[i-1]+s[i];
         second[i] = second[i-1]+s[i]*s[i];
@@ -24,7 +26,7 @@ bool check(int mid){
 int main(){
     cin >> n >> k >> T;
     for (int i=1; i<=n; i++) cin >> s[i];
-    int l = 0, r = n+1;
+    int l = 1, r = n;
 
 
     while (l < r){
@@ -34,6 +36,6 @@ int main(){
         else l = mid+1;
     }
 //    cout << "\n\n" << l << " " << r << "\n";
-    if (l==0) cout << -1;
-    else cout << r;
+    if (check(l)) cout << l;
+    else cout << -1;
 }
