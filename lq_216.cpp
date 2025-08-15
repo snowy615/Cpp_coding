0530128f0902615ab2ca8e1fac6ad19abc36248d
snowy615 <yansnow615@gmail.com>
2025-08-14 20:32:34 -0700

    mem

--- lq_216.cpp
+++ lq_216.cpp
@@ -1,2 +1,45 @@
 #include <iostream>
+#include <cstring>
+
 using namespace std;
+using ll = long long;
+int n, m, k;
+
+int cx[2] = {1, 0};
+int cy[2] = {0, 1};
+const int N = 52;
+
+ll mm[N][N];
+ll dp[N][N][13][13];
+ll d = 1e9+7;
+
+ll test(int x, int y, ll max, int cnt){
+    ll res = 0;
+    if (x == m && y == n) return (ll) (cnt==k);
+    if (dp[x][y][max][cnt]!=-1) return dp[x][y][max][cnt] % d;
+
+    for (int i=0; i<2; i++){
+        int xx = cx[i]+x;
+        int yy = cy[i]+y;
+        if (xx > 0 && xx <= n && yy > 0 && yy<=m){
+            res = (res+test(xx, yy, max, cnt))%d;
+            if (mm[xx][yy] > max && cnt < k) {
+                res = (res + test(xx, yy, mm[xx][yy], cnt+1))%d;
+            }
+        } else continue;
+    }
+    return dp[x][y][max][cnt] = res;
+
+}
+
+int main(){
+    memset(dp, -1, sizeof dp);
+    cin >> n >> m >> k;
+    for (int i=1; i<=n; i++){
+        for (int j=1; j<=m; j++){
+            cin >> mm[i][j];
+            mm[i][j] ++;
+        }
+    }
+    cout << (test(1, 1, 0, 0) + test(1,1,mm[1][1], 1))%d;
+}
