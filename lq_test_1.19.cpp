af1a277d8142a03683278f572847df910da69ac6
snowy615 <yansnow615@gmail.com>
2025-08-14 22:20:39 -0700

    4th

--- lq_test_1.19.cpp
+++ lq_test_1.19.cpp
@@ -5,12 +5,18 @@ using ll = long long;
 const int N = 1e5+5;
 int n, k, T;
 ll s[N];
-ll first[N], second[N];
+
 
 //var = E(x^2) - [E(x)]^2
 bool check(int mid){
+    ll first[mid+1], second[mid+1];
+    sort(s, s+mid+1);
+    for (int i=1; i<=n; i++){
+        first[i] = first[i-1]+s[i];
+        second[i] = second[i-1]+s[i]*s[i];
+    }
     for (int i=0; i<=mid-k; i++){
-        if ((second[i+k]-first[i+k]*first[i+k]/k)< T*k) return true;
+        if ((second[i+k]-second[i])*k*k-(first[i+k]*first[i+k]-first[i]*first[i]) < T) return true;
     }
     return false;
 }
@@ -20,15 +26,12 @@ int main(){
     for (int i=1; i<=n; i++) cin >> s[i];
     int l = 0, r = n+1;
 
-    for (int i=1; i<=n; i++){
-        first[i] = first[i-1]+s[i];
-        second[i] = second[i-1]+s[i]*s[i];
-    }
-    while (l +1 < r){
+
+    while (l < r){
         int mid = (l+r) >> 1;
 //        cout << l << " " << r << "\n";
         if (check(mid)) r = mid;
-        else l = mid;
+        else l = mid+1;
     }
 //    cout << "\n\n" << l << " " << r << "\n";
     if (l==0) cout << -1;
