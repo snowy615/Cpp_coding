fd05ecb43b1acc6ddc4271e09e0aa35d47774b51
snowy615 <yansnow615@gmail.com>
2025-08-15 21:17:07 -0700

    Use of Lucas theorem

--- lq_test_1.24.cpp
+++ lq_test_1.24.cpp
@@ -1,26 +1,31 @@
+
+//int nCr(int r){
+//    double sum = 1;
+//    // Calculate the value of n choose r using the binomial coefficient formula
+//    for (int i = 1; i <= r; i++){
+//        sum = sum * (n - r + i) / i;
+//    }
+//    int s = (int)sum;
+//    return s%2;
+//}
+
 #include <iostream>
 using namespace std;
-int n;
-const int N = 1e5+5;
-int num[N];
-int nCr(int r){
-    double sum = 1;
-    // Calculate the value of n choose r using the binomial coefficient formula
-    for (int i = 1; i <= r; i++){
-        sum = sum * (n - r + i) / i;
-    }
-    int s = (int)sum;
-    return s%2;
-}
 
-int main(){
+const int N = 1e5 + 5;
+int num[N];
+
+int main() {
+    int n;
     cin >> n;
-    n --;
+    for (int i = 0; i < n; i++) cin >> num[i];
+    n--;
     int ans = 0;
-    for (int i=0; i<=n; i++){
-        cin >> num[i];
-        if (nCr(i)) ans = ans ^ num[i];
+    for (int i = 0; i <= n; i++) {
+        // Use Lucas theorem: C(n, i) mod 2 == 1 iff (i & n) == i
+        if ( (i & n) == i ) {
+            ans ^= num[i];
+        }
     }
     cout << ans;
 }
-
