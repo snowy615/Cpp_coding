eab04ae535b5a19555b1cb5f7b472bf88360b043
snowy615 <yansnow615@gmail.com>
2025-08-16 20:25:23 -0700

    round not working. better to use printf

--- lq_1367.cpp
+++ lq_1367.cpp
@@ -37,5 +37,54 @@ int main(){
         }
         T *= K;
     }
-   cout << round(ans);
+   printf("%.0lf",ans);;
 }
+
+
+
+//
+//#include <bits/stdc++.h>
+//using namespace std;
+//const int N=1e5+9,L=2;
+//const double k=0.993,ET=1e-9;
+//int n;
+//double a[N][2],T=1e5,x=0,y=0;
+//double getans(double x,double y)
+//{
+//    double ans=0;
+//    for(int i=1;i<=n;i++)
+//    {
+//        ans+=sqrt((x-a[i][0])*(x-a[i][0])+(y-a[i][1])*(y-a[i][1]));
+//    }
+//    return ans;
+//}
+//double sa()
+//{
+//    double E=getans(x,y);
+//    double ans=E;
+//    while(T>ET)
+//    {
+//        for(int i=1;i<=L;i++)
+//        {
+//            double nx=x+(double)(rand()*2-RAND_MAX)*T;
+//            double ny=y+(double)(rand()*2-RAND_MAX)*T;
+//            double nE=getans(nx,ny);
+//            double dE=nE-E;
+//            if(dE<0||(exp(-dE/T)>(rand()/RAND_MAX)))
+//            {
+//                x=nx,y=ny;
+//                E=nE;
+//                ans=min(ans,E);
+//            }
+//        }
+//        T*=k;
+//    }
+//    return ans;
+//}
+//int main()
+//{
+//    cin>>n;
+//    for(int i=1;i<=n;i++)cin>>a[i][0]>>a[i][1];
+//    printf("%.0lf",sa());
+//    return 0;
+//}
