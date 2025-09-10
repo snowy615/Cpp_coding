d0bcc2c2967f7ffb06cc2d81c01ea64b2f4b2118
snowy615 <yansnow615@gmail.com>
2025-09-09 20:57:44 -0700

    gesp

--- gesp_1.2.cpp
+++ gesp_1.2.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <algorithm>
+#include <numeric>
 using namespace std;
-int gcd(int a, int b){
-    while (b){
-        gcd(max(a-b, b), min(a-b, b));
+
+int _gcd(int a, int b){
+    if (b>a) {
+        return _gcd(b, a);
     }
-    return a;
+    if (b==0) return a;
+    return _gcd(a%b, b);
 }
 int main(){
     int m, n;
     cin >> m >> n;
+    cout << m*n/_gcd(m, n);
 
+    return 0;
 }
