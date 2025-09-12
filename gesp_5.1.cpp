e4def749d2e62974d31a460ad6f2dc36ea44ce87
snowy615 <yansnow615@gmail.com>
2025-09-12 16:49:39 -0700

    gesp TLE

--- gesp_5.1.cpp
+++ gesp_5.1.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
-#include <algorithm>
+#define int long long
+
 using namespace std;
 int a,b,n,m;
-int ma, mb, nb;
-int main(){
+int ma, mb, nb, tmp;
+signed main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
     cin >> n >> m;
     cin >> a >> b;
 
-    if (a>b) swap(a, b);
-    if (m>n) swap(m, n);
+    if (a>b) {
+        tmp = a;
+        a=b;
+        b=tmp;
+    }
+    if (m>n) {
+        tmp = m;
+        m=n;
+        n=tmp;
+    }
 
     int cnt = 0;
     for (int i=0; i<=m/b; i++){
