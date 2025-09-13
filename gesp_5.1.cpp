00ccbd71f9bb8149972c0c78e13c9042756b9a1c
snowy615 <yansnow615@gmail.com>
2025-09-13 09:12:55 -0700

    gesp passed

--- gesp_5.1.cpp
+++ gesp_5.1.cpp
@@ -4,6 +4,14 @@
 
 using namespace std;
 int a,b,n,m;
+bool check(int cnt){
+    if (cnt <= 0) return true;
+    if (a*cnt > n || a*cnt >m) return false;
+    int ma = (m-a*cnt)/(b-a); //max
+    int mi = 0; //min
+    if (b*cnt > n) mi = (b*cnt-n+b-a-1) / (b-a);
+    return max(0LL, mi) <= min(cnt, ma);
+}
 signed main(){
     cin >> n >> m;
     cin >> a >> b;
@@ -15,7 +23,7 @@ signed main(){
         int l = 0, r=1e9+5;
         while (l+1<r) {
             cnt = (l + r) >> 1;
-            if (((double)m - (double)a * (double)cnt) / ((double)b - (double)a) >= ((double)b * (double)cnt - (double)n) / ((double)b - (double)a)) l = cnt;
+            if (check(cnt)) l = cnt;
             else r = cnt;
         }
         cout << l;
