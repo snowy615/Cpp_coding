943f94bab5117f5f38b2d3cb6fa37d9d1a855097
snowy615 <yansnow615@gmail.com>
2025-09-13 10:05:23 -0700

    gesp passed

--- gesp_5.2.cpp
+++ gesp_5.2.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 int cgcd;
-int b, c, x;
+int b, c, x, px;
 
 int gcd(int aa, int bb){
     if (aa>bb) swap(aa, bb);
 
     if (aa==0) return bb;
-    else return gcd(aa, bb%aa);
+    else if (aa==1) return 1;
+    else return gcd(bb%aa, aa);
 }
 
 int main(){
@@ -18,16 +20,17 @@ int main(){
     if (n==1) {
         for (int i=1; i<=q; i++) cout << a+i << "\n";
     }else{
-
         cin >> b;
         if (n==2){
-            cgcd = b-a;
+            cgcd = abs(b-a);
         }else{
             cin >> c;
-            cgcd = gcd(b-a, c-b);
+            px  = c;
+            cgcd = gcd(abs(b-a), abs(c-b));
             for (int i=3; i<n; i++){
                 cin >> x;
-                cgcd = gcd(x, cgcd);
+                cgcd = gcd(abs(x-px), cgcd);
+                px = x;
             }
         }
 
