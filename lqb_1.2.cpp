128dadb43c5d6621f88bc0e04894614a82e5dcc0
snowy615 <yansnow615@gmail.com>
2025-09-27 15:04:52 -0700

    wrong

---
+++ lqb_1.2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+using namespace std;
+
+//vector<pair<int, int>> yes;
+vector<pair<int, int>> all;
+
+int main(){
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int n;
+    cin >> n;
+    int a, b;
+    for (int i=1; i<=n; i++){
+        cin >> a >> b;
+        all.push_back({a,b});
+    }
+    sort(all.begin(), all.end(), greater<pair<int, int>>());
+//    yes.push_back(all[0]);
+    int snd_max = all[0].second;
+    int cnt = 1;
+    for (int i=1; i<n; i++){
+        if (snd_max <= all[i].second) {
+            cnt ++;
+            snd_max = all[i].second;
+             //cout << i << "i\n";
+//            yes.push_back(all[i]);
+        }
+    }
+    cout << cnt;
+}
