817b5242d0a70202f1575078b526970a74a27f59
snowy615 <yansnow615@gmail.com>
2025-12-28 21:52:38 -0800

    passed

---
+++ cses coding/weirdalgo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+using ll = long long;
+
+signed main(){
+    ll n;
+    cin >> n;
+    cout << n << " ";
+    while(n!=1){
+        if (n%2) n=3*n+1;
+        else n /= 2;
+        cout << n << " ";
+    }
+}
