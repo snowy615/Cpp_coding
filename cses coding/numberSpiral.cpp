169f68ab522072e93846ff0ba4fa577ca2b43087
snowy615 <yansnow615@gmail.com>
2025-12-29 10:47:40 -0800

    /

--- cses coding/numberSpiral.cpp
+++ cses coding/numberSpiral.cpp
@@ -1,3 +1,20 @@
-//
-// Created by Snow yan on 2025-12-28.
-//
+#include <iostream>
+using namespace std;
+
+using ll = long long;
+
+int main(){
+    ll t;
+    ll y, x;
+    cin >> t;
+    while (t--){
+        cin >> y >> x;
+        if (y>x){
+            if (y%2) cout << (y-1)*(y-1)+x << "\n";
+            else cout << y*y-x+1 << "\n";
+        }else{
+            if(x%2) cout << x*x-y+1 << "\n";
+            else cout << (x-1)*(x-1)+y << "\n";
+        }
+    }
+}
