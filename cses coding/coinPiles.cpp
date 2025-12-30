8337946cd81d31f49a014996fc9af9857e3cf37e
snowy615 <yansnow615@gmail.com>
2025-12-30 08:21:15 -0800

    passed

--- cses coding/coinPiles.cpp
+++ cses coding/coinPiles.cpp
@@ -3,3 +3,13 @@ using namespace std;
 
 using ll = long long;
 // a b remove 1 2 remove 2 1
+
+int main(){
+    ll t, a, b;
+    cin >> t;
+    while (t--){
+        cin >> a >> b;
+        if ((a+b)%3!=0 || a>2*b || b>2*a) cout << "NO\n";
+        else cout << "YES\n";
+    }
+}
