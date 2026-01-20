cca8d60faa675e3833d3dd5779ff3789f99d84d2
snowy615 <yansnow615@gmail.com>
2026-01-20 09:28:16 +0000

    redo

--- cses coding/retype.cpp
+++ cses coding/retype.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
+int n;
+
+void solveHanoi(int total, int source, int target, int auxiliary){
+    if (total==0) return;
+    solveHanoi(total-1, source, auxiliary, target);
+    cout << source << " " << target << "\n";
+    solveHanoi(total-1, auxiliary, target, source);
+
+}
 
 int main(){
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(0);
 
-    int n;
     cin >> n;
-    int t = 1 << n;
-    for (int i = 0; i<t; i++){
-        int gray_val = i^(i+1);
-        for (int k=n-1; k>=0; k--)
-    }
+    cout << (1<<n) - 1 << "\n";
+    solveHanoi(n, 1, 3, 2);
+    return 0;
 
 }
