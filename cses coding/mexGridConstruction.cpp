e6e9546798283f4853d128451accc0167f3b6dcc
snowy615 <yansnow615@gmail.com>
2026-01-24 09:16:59 +0000

    mexgrid

---
+++ cses coding/mexGridConstruction.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    for (int i=0; i<n; i++){
+        for (int j=0; j<n; j++){
+            cout << (i^j);
+            if (j<n-1) cout << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
