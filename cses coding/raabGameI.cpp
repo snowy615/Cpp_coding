87caf031bd4ee44e22e1267bad5c2711542fdef1
snowy615 <yansnow615@gmail.com>
2026-01-24 09:04:14 +0000

    raab

--- cses coding/raabGameI.cpp
+++ cses coding/raabGameI.cpp
@@ -1,50 +1,53 @@
 #include <iostream>
 #include <vector>
-#include <string>
+#include <numeric>
 #include <algorithm>
+
 using namespace std;
 
-int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    
-    int t, n, a, b;
-    cin >> t;
-    while (t--){
-        cin >> n >> a >> b;
-        if (a+b > n) cout << "NO\n";
-        else{
-            vector<int> a(n), b(n);
-            vector<int> fa(n), fb(n);
-            for (int i=1; i<=n; i++){
-                a[i] = i;
-                b[i] = i;
-            }
-            for (int i=n; i>=n-a-b+1; i--){
-                fa.push_back(a[i]);
-                fb.push_back(b[i]);
-                // a[i].erase(a.begin()) not efficient
-                a.pop_back();
-                b.pop_back();
-            }
-            for (int i=0; i<a; i++){
-                fa.push_back(a[0]);
-                fb.push_back(b[b.size()-1]);
-                a.erase(a.begin());
-                b.pop_back();
-            }
-            for (int i=0; i<b; i++){
-                fb.push_back(b[0]);
-                fa.push_back(a[a.size()-1]);
-                b.erase(b.begin());
-                a.pop_back();
-            }
-            cout << "YES\n";
-            for (int i=0; i<fa.size(); i++) cout << fa[i] << " ";
-            cout << "\n";
-            for (int i=0; i<fb.size(); i++) cout << fb[i] << " ";
-            cout << "\n";
-            
-        }
+void solve(){
+    int n, a, b;
+    cin >> n >> a >> b;
+    if (a+b > n) {
+        cout << "NO\n";
+        return;
     }
+    if (a+b>0 && (a==0 || b==0)){
+        cout << "NO\n";
+        return;
+    }
+    cout << "YES\n";
+
+    vector<int> p1, p2;
+    int ties = n-a-b;
+    for (int i=1; i<=ties; i++){
+        p1.push_back(i);
+        p2.push_back(i);
+    }
+    vector<int> rem_cards;
+    for (int i=ties+1; i<=n; i++){
+        rem_cards.push_back(i);
+    }
+    
+    p1.insert(p1.end(), rem_cards.begin(), rem_cards.end());
+    vector<int> p2_rem = rem_cards;
+    if (!rem_cards.empty()){
+        rotate(p2_rem.begin(), p2_rem.begin()+a, p2_rem.end());
+
+    }
+    p2.insert(p2.end(), p2_rem.begin(), p2_rem.end());
+
+    for (int i=0; i<n; i++) cout << p1[i] << (i==n-1 ? "\n" : " ");
+    cout << "\n";
+    for (int i=0; i<n; i++) cout << p2[i] << (i==n-1 ? "\n" : " ");
+    cout << "\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) solve();
+    return 0;
 }
