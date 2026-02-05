4fdc0b9f2a5178e38c853df5095a619bc2959ad4
snowy615 <yansnow615@gmail.com>
2026-02-05 15:52:35 +0000

    passed

---
+++ cses coding/distinctNumbers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    vector<int> numbers(n);
+
+    for (int i = 0; i<n; i++) cin >> numbers[i];
+
+    sort(numbers.begin(), numbers.end());
+    auto last_unique = unique(numbers.begin(), numbers.end());
+    int distinct_cnt = last_unique-numbers.begin();
+    cout << distinct_cnt << "\n";
+    return 0;
+}
