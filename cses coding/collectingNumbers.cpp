c8624d1481e79b6edc1930ed8f00925810834de9
snowy615 <yansnow615@gmail.com>
2026-02-11 16:36:41 +0000

    passed

---
+++ cses coding/collectingNumbers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    vector<int> pos(n+1);
+    for (int i = 0; i < n; i++){
+        int val;
+        cin >> val;
+        pos[val] = i;
+    }
+    //index of order
+    int rounds = 1;
+    for (int i = 1; i<n; i++){
+        if (pos[i+1] < pos[i]) rounds++; //in front
+    }
+    cout << rounds;
+    return 0;
+
+}
