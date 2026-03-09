45f8d4e8a7b3136f748b7604f8c42dd3e094d293
snowy615 <yansnow615@gmail.com>
2026-03-09 15:52:16 +0000

    passed

--- cses coding/josephusProblemII.cpp
+++ cses coding/josephusProblemII.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
-
+//pbds = policy based data structure
 using namespace std;
 using namespace __gnu_pbds;
 
+//key, value = null, comparator = ascending, tree type = red black tree, node update policy = order statistics
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+//order statistic tree
 
 int main(){
     ios_base::sync_with_stdio(0);
