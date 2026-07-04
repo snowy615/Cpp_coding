a3023b62eefaa61ee6850fa360ce352a369d4094
snowy615 <yansnow615@gmail.com>
2026-07-04 14:58:57 +0100

    passed

---
+++ usaco/b6/b6_sleepyCowSorting.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    vector<int> p(n);
+    for (int i = 0; i < n; i++) cin >> p[i];
+
+    int sorted_suffix_len = 1;
+    for (int i = n-2; i >= 0; i--) {
+        if (p[i] < p[i+1]) sorted_suffix_len ++;
+        else break;
+    }
+    cout << n-sorted_suffix_len;
+    return 0;
+}
