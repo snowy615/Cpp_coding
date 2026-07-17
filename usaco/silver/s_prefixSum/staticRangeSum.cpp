81ef83bcdf0e957efcd42b6f7668cca2bb1a7c65
snowy615 <yansnow615@gmail.com>
2026-07-17 19:45:13 +0100

    passed

---
+++ usaco/silver/s_prefixSum/staticRangeSum.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n, q;
+    cin >> n >> q;
+    ll a;
+    vector<ll> p(n+1, 0);
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        p[i+1] = p[i] + a;
+    }
+    int l, r;
+    for (int i = 0; i < q; i++) {
+        cin >> l >> r;
+        cout << p[r]-p[l] << "\n";
+    }
+    return 0;
+}
